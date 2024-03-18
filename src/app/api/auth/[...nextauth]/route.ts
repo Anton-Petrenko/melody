82a00844881ed8719aacc9cb9c487685058b1eff
Anton-Petrenko <antonp.send@gmail.com>
2024-03-18 17:32:17 -0400

    deployment compatibility update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-import Spotify from "next-auth/providers/spotify"
-
-const scope = "user-read-playback-state streaming playlist-read-private user-follow-read user-top-read user-read-recently-played user-library-read user-read-email"
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    Spotify ({
-      clientId: process.env.SPOTIFY_CLIENT_ID as string,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
-      authorization: {
-          params: {
-              scope: scope,
-          }
-      },
-  })
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async jwt({ token, account }){
-      if (account) {
-        token.accessToken = account.access_token
-      }
-      return token
-    },
-    async session({ session, token }) {
-      session.user = token
-      return session
-    }
-  }
-}
+import NextAuth from "next-auth"
+import { authOptions } from "@/app/utils/auth";
 
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST }
---
+++ src/app/utils/auth.ts
@@ -0,0 +1,31 @@
+import { NextAuthOptions } from "next-auth"
+import Spotify from "next-auth/providers/spotify"
+
+const scope = "user-read-playback-state streaming playlist-read-private user-follow-read user-top-read user-read-recently-played user-library-read user-read-email"
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+      Spotify ({
+        clientId: process.env.SPOTIFY_CLIENT_ID as string,
+        clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
+        authorization: {
+            params: {
+                scope: scope,
+            }
+        },
+    })
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+      async jwt({ token, account }){
+        if (account) {
+          token.accessToken = account.access_token
+        }
+        return token
+      },
+      async session({ session, token }) {
+        session.user = token
+        return session
+      }
+    }
+  }
